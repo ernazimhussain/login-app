14533b59f44227e113fa9f3c90051c7b3cc46195
Nazim Hussain <49742929+ernazimhussain@users.noreply.github.com>
2022-08-31 18:37:54 +0530

    init

---
+++ src/main/kotlin/com/naaz/omsapp/OmsAppApplication.kt
@@ -0,0 +1,11 @@
+package com.naaz.omsapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OmsAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<OmsAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/naaz/omsapp/OmsAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.naaz.omsapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OmsAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
